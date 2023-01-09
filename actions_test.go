72b4a3f3ddd698ce17792bc9d129ea1b38b66c47
Ben Castan <ben@castanfamily.com>
2023-01-09 17:38:01 +1100

    actions_test.go

--- actions_test.go
+++ actions_test.go
@@ -6,5 +6,18 @@ import (
 )
 
 func TestFilterOut(T *TESTING.T {
+	testCases := []struct {
+		name	string
+		file	string
+		ext		string
+		minSize	int64
+		expected	bool
+		}{
+			{"FilterNoExtension", "testdata/dir.log", "", false},
+			{"FilterExtensionMatch", "testdata/dir.log", 0, false},
+			{"FilterExtensionNoMatch", "testdir/dir.log", ".sh"0, true},
+			{"FilterExtensionSizeMatch", "testdata/dir.log", 10, false},
+			{"FilterExtensionSizeNoMatch", "testdata/dir.log", 20, true},
+		}
 
 }
